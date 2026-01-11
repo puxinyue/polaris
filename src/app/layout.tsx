47604c84c5916d7ac47295ebc66991acdb74e30c
puxinyu189 <2467388553@qq.com>
2026-01-11 15:22:45 +0800

    feat: step02 auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter,IBM_Plex_Mono } from "next/font/google";
+import { Inter,IBM_Plex_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import "./globals.css";
-
+import { dark } from '@clerk/themes'
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -25,6 +33,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
@@ -35,9 +48,23 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
